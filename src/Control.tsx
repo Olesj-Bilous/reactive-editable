561e58d16f34aeb65209117757c238a6504d89b4
Olesj-Bilous <olesjvbilous@gmail.com>
2023-10-23 16:59:44 +0200

    unmemo control

--- src/Control.tsx
+++ src/Control.tsx
@@ -1,48 +1,42 @@
-import { ReactNode, memo } from 'react'
+import { ReactNode } from 'react'
 import { useEditControl, useEditEffects } from './Context'
 
-export const ToggleEdit = memo(
-  ({ children, className }: { children?: ReactNode, className?: string }) => {
-    const { toggleEdit } = useEditEffects()
+export function ToggleEdit({ children, className }: { children?: ReactNode, className?: string }) {
+  const { toggleEdit } = useEditEffects()
 
-    return (
-      <button className={className ?? 'toggle-edit'} onClick={toggleEdit}>
-        {children}
-      </button>
-    )
-  }
-)
-
-export const Revert = memo(
-  ({ children, className }: { children?: ReactNode, className?: string }) => {
-    const { isTouched } = useEditControl()
-    const { revert } = useEditEffects()
-
-    return <button className={className ?? 'revert'} disabled={isTouched} onClick={revert}>
+  return (
+    <button className={className ?? 'toggle-edit'} onClick={toggleEdit}>
       {children}
     </button>
-  }
-)
+  )
+}
 
-export const Save = memo(
-  ({ children, className }: { children?: ReactNode, className?: string }) => {
-    const { isTouched, isValid } = useEditControl()
-    const { save } = useEditEffects()
 
-    return <button className={className ?? 'save'} disabled={!isTouched || !isValid} onClick={save}>
-      {children}
-    </button>
-  }
-)
+export function Revert({ children, className }: { children?: ReactNode, className?: string }) {
+  const { isTouched } = useEditControl()
+  const { revert } = useEditEffects()
 
-export const Remove = memo(
-  ({ children, className }: { children?: ReactNode, className?: string }) => {
-    const { mode } = useEditControl()
-    const { remove } = useEditEffects()
+  return <button className={className ?? 'revert'} disabled={isTouched} onClick={revert}>
+    {children}
+  </button>
+}
 
-    return <>{mode !== 'create' && remove && <button className={className ?? 'remove'} onClick={remove}>
-      {children}
-    </button>}</>
-  }
-)
 
+export function Save({ children, className }: { children?: ReactNode, className?: string }) {
+  const { isTouched, isValid } = useEditControl()
+  const { save } = useEditEffects()
+
+  return <button className={className ?? 'save'} disabled={!isTouched || !isValid} onClick={save}>
+    {children}
+  </button>
+}
+
+
+export function Remove({ children, className }: { children?: ReactNode, className?: string }) {
+  const { mode } = useEditControl()
+  const { remove } = useEditEffects()
+
+  return <>{mode !== 'create' && remove && <button className={className ?? 'remove'} onClick={remove}>
+    {children}
+  </button>}</>
+}
