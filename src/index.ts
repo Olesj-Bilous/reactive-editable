bd1dd035dfb4f107ad7aac2bab71781dc7d6fce2
Olesj-Bilous <olesjvbilous@gmail.com>
2023-10-26 01:25:31 +0200

    export useEditor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { EditPermissionContext, useEditPermissionContext, useEditControl, useEditEffects } from './Context'
 import { Editable } from './Provider'
 import { ToggleEdit, Revert, Save, Remove } from './Control'
+import { useEditor } from './useEditor'
 
 export { EditPermissionContext, useEditPermissionContext, useEditControl, useEditEffects }
 export { Editable }
 
 export { ToggleEdit, Revert, Save, Remove }
+
+export { useEditor }
