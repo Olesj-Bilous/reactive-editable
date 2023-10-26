c14909a45718a3045540dc2fd64d4bdba6e30a07
Olesj-Bilous <olesjvbilous@gmail.com>
2023-10-26 16:32:40 +0200

    export types

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import { Editable } from './Provider'
 import { ToggleEdit, Revert, Save, Remove } from './Control'
 import { useEditor } from './useEditor'
 
+import type { EditableControl, EditableEffects } from "./Context";
+import type { EditConfig } from "./Provider";
+import type { Settable } from "./useEditor";
+
 export { EditPermissionContext, useEditPermissionContext, useEditControl, useEditEffects }
 export { Editable }
 
 export { ToggleEdit, Revert, Save, Remove }
 
 export { useEditor }
+
+export { EditableControl, EditableEffects }
+export { EditConfig }
+export { Settable }
